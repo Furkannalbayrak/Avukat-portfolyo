54204f6aa8df47f4c9c0d855c23e6f651f3a1afb
Furkan Albayrak <furkanalb42@gmail.com>
2025-06-10 20:29:57 +0300

    New app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import About from './components/About'
+import Services from './components/Services'
+import Articles from './components/Articles'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 function App() {
-
   return (
-    <div>
-        <div>
-          <Header/>
-        </div>
+    <div className="min-h-screen bg-gray-50">
+      <Header />
+      
+      <main>
+        <About />
+        <Services />
+        <Articles />
+        <Contact />
+      </main>
+
+      <Footer />
     </div>
   )
 }
