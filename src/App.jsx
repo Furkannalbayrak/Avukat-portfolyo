a1c9bb5893900b9aea4227be23650fec274d506d
Furkan Albayrak <furkanalb42@gmail.com>
2025-06-10 18:17:08 +0300

    app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <div>
+        <div>
+          <Header/>
+        </div>
+    </div>
+  )
+}
+
+export default App
